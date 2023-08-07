eaafe6e9a24e93511296c85b406588441cddf713
Alina <alivka@MacBook-Air-Alina.local>
2023-08-07 23:49:07 +0300

    Add brain_even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,52 @@
+import random
+import prompt
+from brain_games.scripts.brain_games import greet
+
+
+ROUNDS = 3
+START_RANGE = 1
+STOP_RANGE = 100
+
+
+# проверяем число на четность
+def is_even(num):
+	if num % 2 == 0:
+		res = 'yes'
+	else:
+		res = 'no'
+	return res
+
+
+def game():
+	# рассказываем правила игры
+	print('Answer "yes" if the number is even, otherwise answer "no".')
+
+	# играем с пользователем
+	for x in range(ROUNDS):
+		# генерирум случайное число
+		number = random.randint(START_RANGE, STOP_RANGE)
+
+		# узнаем у пользователя ответ
+		print(f'Question: {number}')
+		answer = prompt.string('Your answer: ')
+
+		# сверяем с правильным
+		result = is_even(number)
+
+		# показываем результат
+		if answer == result:
+			print('Correct!')
+		else:
+			print(f"{answer} is wrong answer ;(. Correct answer was {result}. Let's try again, Alina!")
+			break
+	else:
+		print(f'Congratulations, Alina!')
+
+
+def main():
+	greet()
+	game()
+
+
+if __name__ == '__main__':
+	main()
